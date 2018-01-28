66c8c2adf3511f3c219f1202a30d0cdd23dfa359
Scott Brady <scottbrady91@outlook.com>
2018-01-28 09:29:45 +0000

    Initial project from template

---
+++ ScottBrady91.Civic.AspNetCore.Example/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ScottBrady91.Civic.AspNetCore.Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
