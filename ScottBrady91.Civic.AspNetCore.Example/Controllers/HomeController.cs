ccd4ef8b6f3deb2499e14848aea4f1f25329b199
Scott Brady <scottbrady91@outlook.com>
2018-02-04 17:08:33 +0000

    Brought example structure inline with blog post

--- ScottBrady91.Civic.AspNetCore.Example/Controllers/HomeController.cs
+++ ScottBrady91.Civic.AspNetCore.Example/Controllers/HomeController.cs
@@ -51,142 +51,95 @@ namespace ScottBrady91.Civic.AspNetCore.Example.Controllers
         {
             if (string.IsNullOrWhiteSpace(model?.AuthCode)) return BadRequest();
 
+            // CREATING THE JWT
+            // your private signing key from Civic
             const string privateKey = "e8593ad98db1dda0f57c16ef1f53c4c6b57fa35d9b5f82602353ccfb5a71f047";
-            const string civicAuthServerPublicKey = "049a45998638cfb3c4b211d72030d9ae8329a242db63bfb0076a54e7647370a8ac5708b57af6065805d5a6be72332620932dbb35e8d318fce18e7c980a0eb26aa1";
-            const string secret = "482283244e2e8082d6f1c3ef288930ce";
-
-            // generate JWT - sign using private key & ECDSA
-            var token = CreateSignedJwt(LoadPrivateKey(FromHexString(privateKey)));
-            
-            // generate body
-            var data = JsonConvert.SerializeObject(new { authToken = model.AuthCode });
-
-            // HMAC of body
-            var hash = CreateHash(secret, data);
-
-            // exchange auth code
-            var httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Civic", $"{token}.{hash}");
-            var responseMessage = await httpClient.PostAsync("https://api.civic.com/sip/prod/scopeRequest/authCode",
-                new StringContent(data, Encoding.UTF8, "application/json"));
-
-            var response = await responseMessage.Content.ReadAsStringAsync();
-
-            // extract data (JWT) from response
-            var jObject = JObject.Parse(response);
-            var jToken = jObject["data"];
-            var jwt = jToken.Value<string>();
-
-            // verify & extract jwt
-            var claimsPrincipal = VerifySignedJwt(LoadPublicKey(FromHexString(civicAuthServerPublicKey)), jwt);
-
-            // decrypt data
-            var loadedData = claimsPrincipal.FindFirst("data");
-            var userData = DecryptData(secret, loadedData.Value);
-
-            // sign in user
-            var claimsIdentity = new ClaimsIdentity(userData.Select(x => new Claim(x.Label, x.Value)).ToList(), "cookie");
-            claimsIdentity.AddClaim(new Claim("userId", jObject["userId"].Value<string>()));
-
-            await HttpContext.SignInAsync("cookie", new ClaimsPrincipal(claimsIdentity));
-
-            return Ok();
-        }
-
-        private static byte[] FromHexString(string hex)
-        {
-            var numberChars = hex.Length;
-            var hexAsBytes = new byte[numberChars / 2];
-            for (var i = 0; i < numberChars; i += 2)
-                hexAsBytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
-
-            return hexAsBytes;
-        }
-
-        private static ECDsa LoadPrivateKey(byte[] key)
-        {
-            var privKeyInt = new Org.BouncyCastle.Math.BigInteger(+1, key);
+            var privKeyInt = new Org.BouncyCastle.Math.BigInteger(+1, FromHexString(privateKey));
             var parameters = SecNamedCurves.GetByName("secp256r1");
             var qa = parameters.G.Multiply(privKeyInt);
-            var pubKeyX = qa.Normalize().XCoord.ToBigInteger().ToByteArrayUnsigned();
-            var pubKeyY = qa.Normalize().YCoord.ToBigInteger().ToByteArrayUnsigned();
+            var privKeyX = qa.Normalize().XCoord.ToBigInteger().ToByteArrayUnsigned();
+            var privKeyY = qa.Normalize().YCoord.ToBigInteger().ToByteArrayUnsigned();
 
-            return ECDsa.Create(new ECParameters
+            var privateKeyEcdsa = ECDsa.Create(new ECParameters
             {
                 Curve = ECCurve.NamedCurves.nistP256,
                 D = privKeyInt.ToByteArrayUnsigned(),
+                Q = new ECPoint
+                {
+                    X = privKeyX,
+                    Y = privKeyY
+                }
+            });
+
+            var now = DateTime.UtcNow;
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var signingCredentials = new SigningCredentials(new ECDsaSecurityKey(privateKeyEcdsa), SecurityAlgorithms.EcdsaSha256);
+
+            var jwtHeader = new JwtHeader(signingCredentials);
+            var jwtPayload = new JwtPayload(
+                issuer: "rJ3fVI9rz",
+                audience: "https://api.civic.com/sip",
+                claims: new List<Claim> { new Claim("sub", "rJ3fVI9rz"), new Claim("jti", Guid.NewGuid().ToString()) },
+                notBefore: null,
+                expires: now.AddMinutes(5),
+                issuedAt: now);
+            jwtPayload.Add("data", new Dictionary<string, string> { { "method", "POST" }, { "path", "scopeRequest/authCode" } });
+
+            var exchangeToken = tokenHandler.WriteToken(new JwtSecurityToken(jwtHeader, jwtPayload));
+
+            // CREATING THE MESSAGE DIGEST
+            // your secret from Civic
+            const string secret = "482283244e2e8082d6f1c3ef288930ce";
+
+            var data = JsonConvert.SerializeObject(new { authToken = model.AuthCode });
+
+            var secretBytes = Encoding.Default.GetBytes(secret);
+            var dataBytes = Encoding.Default.GetBytes(data);
+            var hmac = new HMACSHA256(secretBytes);
+            var hashAsBytes = hmac.ComputeHash(dataBytes);
+            var hash = Convert.ToBase64String(hashAsBytes);
+
+            // REQUESTING USER DATA
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Civic", $"{exchangeToken}.{hash}");
+            var responseMessage = await httpClient.PostAsync("https://api.civic.com/sip/prod/scopeRequest/authCode", new StringContent(data, Encoding.UTF8, "application/json"));
+
+            var response = await responseMessage.Content.ReadAsStringAsync();
+
+            // VERIFYING THE JWT SIGNATURE
+            const string civicAuthServerPublicKey = "049a45998638cfb3c4b211d72030d9ae8329a242db63bfb0076a54e7647370a8ac5708b57af6065805d5a6be72332620932dbb35e8d318fce18e7c980a0eb26aa1";
+            var pubKeyX = FromHexString(civicAuthServerPublicKey).Skip(1).Take(32).ToArray();
+            var pubKeyY = FromHexString(civicAuthServerPublicKey).Skip(33).ToArray();
+
+            var publicKeyEcdsa = ECDsa.Create(new ECParameters
+            {
+                Curve = ECCurve.CreateFromFriendlyName("secp256r1"),
                 Q = new ECPoint
                 {
                     X = pubKeyX,
                     Y = pubKeyY
                 }
             });
-        }
 
-        private static ECDsa LoadPublicKey(byte[] key)
-        {
-            var x = key.Skip(1).Take(32).ToArray();
-            var y = key.Skip(33).ToArray();
+            var jObject = JObject.Parse(response);
+            var jToken = jObject["data"];
+            var jwt = jToken.Value<string>();
 
-            return ECDsa.Create(new ECParameters
-            {
-                Curve = ECCurve.CreateFromFriendlyName("secp256r1"),
-                Q = new ECPoint
-                {
-                    X = x,
-                    Y = y
-                }
-            });
-        }
-
-        private static string CreateSignedJwt(ECDsa eCDsa)
-        {
-            var now = DateTime.UtcNow;
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var signingCredentials = new SigningCredentials(new ECDsaSecurityKey(eCDsa), SecurityAlgorithms.EcdsaSha256);
-
-            var jwtHeader = new JwtHeader(signingCredentials);
-            var jwtPayload = new JwtPayload(
-                issuer: "rJ3fVI9rz",
-                audience: "https://api.civic.com/sip",
-                claims: new List<Claim> {new Claim("sub", "rJ3fVI9rz"), new Claim("jti", Guid.NewGuid().ToString())},
-                notBefore: null,
-                expires: now.AddMinutes(5),
-                issuedAt: now);
-            jwtPayload.Add("data", new Dictionary<string, string> { { "method", "POST" }, { "path", "scopeRequest/authCode" } });
-
-            return tokenHandler.WriteToken(new JwtSecurityToken(jwtHeader, jwtPayload));
-        }
-
-        private static string CreateHash(string secret, string data)
-        {
-            var secretBytes = Encoding.Default.GetBytes(secret);
-            var dataBytes = Encoding.Default.GetBytes(data);
-            var hmac = new HMACSHA256(secretBytes);
-            var hash = hmac.ComputeHash(dataBytes);
-            return Convert.ToBase64String(hash);
-        }
-
-        private static ClaimsPrincipal VerifySignedJwt(ECDsa eCDsa, string token)
-        {
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var claimsPrincipal = tokenHandler.ValidateToken(token, new TokenValidationParameters
+            var claimsPrincipal = tokenHandler.ValidateToken(jwt, new TokenValidationParameters
             {
                 ValidIssuer = "civic-sip-hosted-service",
                 ValidateIssuer = true,
                 ValidAudience = "https://api.civic.com/sip/",
                 ValidateAudience = true,
                 ValidateLifetime = true,
-                IssuerSigningKey = new ECDsaSecurityKey(eCDsa)
+                IssuerSigningKey = new ECDsaSecurityKey(publicKeyEcdsa)
             }, out var _);
 
-            return claimsPrincipal;
-        }
+            // DECRYPTING THE DATA
+            var loadedData = claimsPrincipal.FindFirst("data").Value;
 
-        private static List<UserData> DecryptData(string secret, string data)
-        {
-            var iv = FromHexString(data.Substring(0, 32));
-            var encryptedData = Convert.FromBase64String(data.Substring(32));
+            var iv = FromHexString(loadedData.Substring(0, 32));
+            var encryptedData = Convert.FromBase64String(loadedData.Substring(32));
 
             string plainTextUserData;
 
@@ -200,7 +153,25 @@ namespace ScottBrady91.Civic.AspNetCore.Example.Controllers
             using (var srDecrypt = new StreamReader(cryptoStream))
                 plainTextUserData = srDecrypt.ReadToEnd();
 
-            return JsonConvert.DeserializeObject<List<UserData>>(plainTextUserData);
+            var userData = JsonConvert.DeserializeObject<List<UserData>>(plainTextUserData);
+
+            // AUTHENTICATING THE USER
+            var claimsIdentity = new ClaimsIdentity(userData.Select(x => new Claim(x.Label, x.Value)).ToList(), "cookie");
+            claimsIdentity.AddClaim(new Claim("userId", jObject["userId"].Value<string>()));
+
+            await HttpContext.SignInAsync("cookie", new ClaimsPrincipal(claimsIdentity));
+            
+            return Ok();
+        }
+
+        private static byte[] FromHexString(string hex)
+        {
+            var numberChars = hex.Length;
+            var hexAsBytes = new byte[numberChars / 2];
+            for (var i = 0; i < numberChars; i += 2)
+                hexAsBytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+
+            return hexAsBytes;
         }
 
         public class LoginModel
